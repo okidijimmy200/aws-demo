3b8968b25819d3c1f2b9a7e5f4ff8ef63e750f89
okidijimmy200 <okidijimmie@gmail.com>
2023-04-18 10:06:46 +0300

    refactor: meat

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 '''get route docs'''
 @app.get("/")
 async def root():
-    return {"message": "Tomato and salads, vegetables"}
+    return {"message": "Tomato and salads, vegetables and meat"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
