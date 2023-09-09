b85b7190ffecaa55b416364df24b1e4de6aab244
Okidi Jimmy <okidijimmie@gmail.com>
2023-09-09 11:55:58 +0300

    commit 8

--- test_main.py
+++ test_main.py
@@ -10,4 +10,4 @@ async def test_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Tomato and salads, vegetables and meat"}
+    assert response.json() == {"message": "Tomato and salads, vegetables and meat, test case 5"}
